1f82fc54e84a85b0bf0357e61a1e24a01134bfa5
Yannick Lansink <76790689+yannicklansink@users.noreply.github.com>
2023-03-17 11:53:57 +0100

    dag 4 - begin met opdracht voor de pauze

--- yannick-lansink-lessen-traineeship/yannick-lansink-lessen-traineeship/Program.cs
+++ yannick-lansink-lessen-traineeship/yannick-lansink-lessen-traineeship/Program.cs
@@ -1,9 +1,46 @@
-﻿namespace yannick_lansink_lessen_traineeship;
+﻿using System.Globalization;
+
+namespace yannick_lansink_lessen_traineeship;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CultureInfo.CurrentCulture = new CultureInfo("nl-BE");
+        Console.WriteLine(Math.Round(10.6542, 3));
+
+
+        int[] nummers = new int[] { 2, 5, 6, 7 };
+        foreach (int nummer in nummers)
+        {
+            Console.WriteLine(nummer);
+        }
+
+        // 1 blauwe piste:
+        // array met de priemgetallen: 2, 3, 5, 7, 11, 13, 17
+        // - afdrukken
+        // - som van alle getallen afdrukken
+        int sumOfArray = PrintSumOfArray();
+
+        // 2 blauwe piste:
+        // methode die het grootste getal uit een array teruggeeft. 
+
+        // 3 rode piste:
+        // maak een "array" met alle tafeluitkomsten
+
+
+
+
     }
+
+    static int PrintSumOfArray(int[] array)
+    {
+        int totalValue = 0;
+        foreach (int nummer in array)
+        {
+            totalValue = +nummer;
+        }
+        return totalValue;
+    }
+
 }
 
