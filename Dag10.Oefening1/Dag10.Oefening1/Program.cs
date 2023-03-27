80c7965f237d538e97d72aa079e390db5cdf28b7
Yannick Lansink <76790689+yannicklansink@users.noreply.github.com>
2023-03-27 13:46:35 +0200

    delegates

---
+++ Dag10.Oefening1/Dag10.Oefening1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Dag10.Oefening1;
+
+
+public delegate string Formatteerder(int getal);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Printer printer = new Printer();
+        Layout layout = new Layout();
+
+        Formatteerder format = new Formatteerder(layout.LieftinckFormaat);
+        
+        printer.Print(53, format);
+    }
+    
+}
+
+public class Printer
+{
+    public void Print(int getal, Formatteerder formatteerder)
+    {
+        Console.WriteLine(formatteerder.Invoke(getal));
+    }
+}
+public class Layout
+{
+    public string LieftinckFormaat(int getal)
+    {
+        return $"Het getal is {getal}";
+    }
+}
+
