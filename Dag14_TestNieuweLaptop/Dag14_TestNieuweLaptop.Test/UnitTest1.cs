9845cc86aafb4c838b42c938e71c086bf6fff944
YannickLansink <yj.lansink@belastingdienst.nl>
2023-03-31 11:03:15 +0200

    test nieuwe laptop

---
+++ Dag14_TestNieuweLaptop/Dag14_TestNieuweLaptop.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace Dag14_TestNieuweLaptop.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Person person = new Person();
+
+            Assert.AreEqual(true, true);
+        }
+    }
+}
---
+++ Dag14_TestNieuweLaptop/Dag14_TestNieuweLaptop/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Dag14_TestNieuweLaptop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Person
+    {
+        int age;
+
+        public int Age
+        {
+            get { return age; }
+        }
+
+
+
+    }
+}
