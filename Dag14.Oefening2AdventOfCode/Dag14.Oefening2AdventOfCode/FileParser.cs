d221f4efb62bad291228a21d955874e8b1d187a1
YannickLansink <YannickLansink@KBCBelastingdiensttrajecten.onmicrosoft.com>
2023-03-31 15:59:59 +0200

    advent of code done

---
+++ Dag14.Oefening2AdventOfCode/Dag14.Oefening2AdventOfCode/FileParser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag14.Oefening2AdventOfCode
+{
+    public class FileParser : IDisposable
+    {
+        private FileStream stream;
+        private string FileName;
+
+        public FileParser(string filename)
+        {
+            FileName = filename;
+            stream = new FileStream(filename, FileMode.Open);
+        }
+
+        public void GetHighestNumberFromFile()
+        {
+            stream.WriteByte(65);
+
+        }
+        public void Dispose()
+        {
+            stream?.Dispose();
+        }
+    }
+}
--- Dag14.Oefening2AdventOfCode/Dag14.Oefening2AdventOfCode/Program.cs
+++ Dag14.Oefening2AdventOfCode/Dag14.Oefening2AdventOfCode/Program.cs
@@ -1,10 +1,57 @@
-﻿namespace Dag14.Oefening2AdventOfCode
+﻿using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text;
+using System.Threading;
+
+namespace Dag14.Oefening2AdventOfCode
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string filePath = @"..\..\..\input-advent-of-code.txt";
+            int highestScore = 0;
+
+            /*
+            using (var stream = new FileParser(filePath))
+            {
+                // doe hier wat je wilt.
+                stream.GetHighestNumberFromFile();
+
+
+            } // hier word .Dispose() aangeroepen
+            */
+
+            // other code
+            using (FileStream fileStream = File.OpenRead(filePath))
+            {
+                using (StreamReader streamReader = new StreamReader(fileStream))
+                {
+                    int currentScore = 0;
+                    while (!streamReader.EndOfStream)
+                    {
+                        
+
+                        string stringLine = streamReader.ReadLine();
+                        if (string.IsNullOrWhiteSpace(stringLine))
+                        {
+                            // end of count. witte regel hier
+                            if (currentScore > highestScore)
+                            {
+                                highestScore = currentScore;
+                            }
+                            currentScore = 0;
+                            continue;
+                        }
+
+                        int line = int.Parse(stringLine);
+                        currentScore += line;
+                    }
+                }
+                Console.WriteLine($"The highest score is: {highestScore}");
+            }
+
+
         }
     }
 }
