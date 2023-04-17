bc5ef1596557192483f278193ff00f2da06eb0f7
YannickLansink <YannickLansink@KBCBelastingdiensttrajecten.onmicrosoft.com>
2023-04-17 11:57:16 +0200

    day24

---
+++ MSSQL/day24-exercises11en12.sql
@@ -0,0 +1,49 @@
+﻿-- Huiswerk 11 en 12. Group by en Having
+USE adventureworks
+GO
+
+/* 🌶 Exercise 11 - GROUP BY, HAVING */
+
+/* 1. Use the table Customer.
+Retrieve per SalesPerson the number of customers where this is at least 140 customers
+(result: 3 rows) */
+select
+	c.SalesPerson
+	, COUNT(c.SalesPerson) as number_of_customers
+from SalesLT.Customer as c
+group by SalesPerson
+having COUNT(c.SalesPerson) > 140
+
+/* 2. Copy paste the previous query.
+Now filter out the companies: 'Greater Bike Store', 'Metro Manufacturing' and 'Corner Bicycle Supply'.ABORT
+(result: 2 rows) */
+
+/* 3. Write a brand new query using the table Customer.
+Show middle names (excluding NULL values) which occur at least 25 times, highest count on top:
+
+  MiddleName  count
+  J.          112
+  M.          75
+  ...         ...
+  E.          25
+
+  (result: 8 rows)
+*/
+
+/* 🌶🌶 Exercise 12 - GROUP BY, HAVING */
+
+/* 1. Use the table SalesOrderDetail to retrieve these details:
+
+  SalesOrderID    total
+  71784           89869,2764
+  71936           79589,616
+  71938           74160,228
+  71783           65683,3681
+  71797           65123,4635
+  71902           59894,2092
+
+  Requirements:
+  * DON'T USE the existing LineTotal column!
+  * Exclude orders with a SalesOrderID less than 71780.
+  * Only show orders with a total of at least 55000. Highest on top.
+*/
---
+++ MSSQL/day24-notes.sql
@@ -0,0 +1,89 @@
+
+
+USE adventureworks -- soort namespace
+GO
+
+/*
+ms transact-sql 
+dat is des microsofts waar statements aan de standaard ansi sql is toegevoegd.
+statements zoals: 
+	- []
+	- TOP
+	- ISNULL()
+*/
+
+/*
+Scheme is een container. Om objecten die bij elkaar horen te groeperen
+Database heeft meerdere schemes.
+Wordt gebruikt om te categoriseren.
+	- Zoals de production afdeling:
+		- Production.Categories
+		- Production.Products
+		- Production.Suppliers
+Ook een tool om permissies toe te kennen op schema niveau. 
+DBO is het standaard schema.
+*/
+
+select
+	sod.SalesOrderID
+	, SUM(sod.OrderQty)			as totaal_orderqty -- aggregation function
+from SalesLT.SalesOrderDetail   as sod
+where sod.SalesOrderID >= 71850
+group by sod.SalesOrderID 
+having SUM(sod.OrderQty) >= 80 -- helaas moet je de sum in having overschrijven
+
+-- subquery
+select
+	*
+from SalesLT.Product as p
+where p.ListPrice = 
+(
+	select 
+		MAX(p.ListPrice)
+	from SalesLT.Product as p
+)
+
+-- TOP met TIES. Returned niet 1, maar 3 want die hebben dezelfde waarde
+-- kan ook met TOP 10 PRECENT
+select TOP 1
+	*
+from SalesLT.SalesOrderDetail as sod
+order by sod.UnitPrice
+
+-- N om expliciet aan te geven wat het data type is.
+select
+	c.FirstName
+	, a.City
+from SalesLT.Customer as c
+inner join SalesLT.CustomerAddress as ca on c.CustomerID = ca.CustomerID
+inner join SalesLT.Address as a on a.AddressID = ca.AddressID
+where a.City = N'london'
+-- where c.LastName = N'logan' -- de N maakt het sneller. Dit kan alleen als je te maken hebt met nvarchar()
+
+-- subquery met meer rijen
+select
+	ca.CustomerID
+	, ca.AddressType
+from SalesLT.CustomerAddress as ca 
+where ca.AddressID in
+(
+	select
+		a.AddressID -- a.addressid moet matchen met ca.addressid
+	from SalesLT.Address as a
+	where a.City = N'London'
+)
+
+-- exists. correlated subquery
+select
+	ca.CustomerID
+	, ca.AddressType
+from SalesLT.CustomerAddress as ca 
+where EXISTS
+(
+	select
+		1
+	from SalesLT.Address as a
+	where a.City = N'London'
+	and a.AddressID = ca.AddressID
+)
+
