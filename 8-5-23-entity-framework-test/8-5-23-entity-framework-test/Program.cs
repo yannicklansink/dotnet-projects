8767643679d365e5f89e9b7d11b3a8d9ad6782b4
yannicklansink <yannick.lansink@live.nl>
2023-05-09 19:57:46 +0200

    d

---
+++ 8-5-23-entity-framework-test/8-5-23-entity-framework-test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _8_5_23_entity_framework_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 8-5-23-framwork-test2/8-5-23-framwork-test2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _8_5_23_framwork_test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
