2aab733896c49a3e916dccd42d6c725b960a97eb
Yannick Lansink <yannick.lansink@live.nl>
2023-05-09 11:10:31 +0200

    enitty framework

---
+++ 9-5-23/9-5-23/MagazijnDbContext.cs
@@ -0,0 +1,32 @@
+﻿using _9_5_23.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_5_23
+{
+    internal class MagazijnDbContext : DbContext
+    {
+        // geef hier aan welke entiteiten je op wilt querien.
+        public DbSet<Voorraad> Voorraad { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionstring =
+                @"Server=localhost,1433;Database=MagazijnDB;User=SA;Password=kpHm4Cfc@;TrustServerCertificate=true";
+            optionsBuilder.UseSqlServer(connectionstring);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Voorraad>(locatie =>
+            {
+                locatie.ToTable("voorraad", "ikea");
+                locatie.Property(l => l.Artikel).HasMaxLength(100);
+            });
+        }
+    }
+}
---
+++ 9-5-23/9-5-23/Program.cs
@@ -0,0 +1,40 @@
+﻿using _9_5_23.Model;
+
+namespace _9_5_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // tuple
+            (string, int) x = ("dit", 2);
+
+            using (var context = new MagazijnDbContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+            Console.WriteLine("Your database has been created");
+
+
+            var voorraad1 = new Voorraad
+            {
+                Artikel = "Muis",
+                Aantal = 5,
+            };
+            var voorraad2 = new Voorraad
+            {
+                Artikel = "Computer",
+                Aantal = 9,
+            };
+
+            using (var context = new MagazijnDbContext())
+            {
+                context.Voorraad.Add(voorraad1);
+                context.Voorraad.Add(voorraad2);
+                context.SaveChanges();
+                Console.WriteLine("Your inserts are done");
+            }
+        }
+    }
+}
