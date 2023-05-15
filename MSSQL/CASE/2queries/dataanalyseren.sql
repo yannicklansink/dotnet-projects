d0b1c7a9d756e20482a8f57d7f7eede7779af9a9
yannicklansink <yannick.lansink@live.nl>
2023-05-15 09:45:45 +0200

    opdracht 2

---
+++ MSSQL/CASE/2queries/dataanalyseren.sql
@@ -0,0 +1,45 @@
+-- ----------------------------------------------------------------------------
+-- Opdracht 2: Queries data analyseren 
+-- ----------------------------------------------------------------------------
+
+USE ReisdocumentenDB;
+GO
+
+
+-- ----------------------------------------------------------------------------
+-- (BLAUWE PISTE)
+-- ----------------------------------------------------------------------------
+
+-- 1. Een lijst van alle burgers en hun reisdocument(en) inclusief burgers zonder reisdocumenten.
+
+-- 2. Een lijst van de medewerkers die nog nooit een reisdocument aanvraag hebben behandeld.
+
+-- 3. Het aantal aanvragen uit specifiek de vorige maand gegroepeerd per type.
+
+-- 4. Een lijst van alle burgers en medewerkers die in Hamelen wonen.
+
+
+
+-- ----------------------------------------------------------------------------
+-- (RODE PISTE)
+-- ----------------------------------------------------------------------------
+
+-- 1. Totaalbedrag van alle reisdocumenten per maand
+
+-- 2. Alle documenten die in de komende twee maanden verlopen zijn
+
+-- 3. Per document het type, de status en het aantal
+
+-- 4. Gegevens van elke werknemer met daarbij de gegevens van diens leidinggevende
+
+
+
+-- ----------------------------------------------------------------------------
+-- (ZWARTE PISTE)
+-- ----------------------------------------------------------------------------
+
+-- 1. Per jaar per documenttype het totaalbedrag dat werkelijk betaald is (de prijs van toen) en het totaalbedrag op basis van de huidige prijs (een fictief totaal)
+
+-- 2. Per maand de procentuele toe- of afname van opgehaalde reisdocumenten.
+
+-- 3. Challenge: Per week de eerste 3 opgehaalde reisdocumenten
