26b5d06e1d5f01ac0a34815e32f670eaa9fd5ab6
Yannick Lansink <yannick.lansink@live.nl>
2023-06-09 16:14:39 +0200

    mistake

--- blok3/Dag8.oefening1.solution/Dag8.oefening1/Dal/TodoContext.cs
+++ blok3/Dag8.oefening1.solution/Dag8.oefening1/Dal/TodoContext.cs
@@ -6,20 +6,23 @@ namespace Dag8.oefening1.Dal
 {
     public class TodoContext : DbContext
     {
-
+        private readonly IConfiguration _config;
         public DbSet<Todo> Todos { get; set; }
 
-        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        public TodoContext(DbContextOptions<TodoContext> options, IConfiguration config) : base(options)
         {
-
+            _config = config;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
 
-            var connectionstring = @"Server=localhost,1433;Database=dotnet7todoappdb;User=sa;Password=kpHm4Cfc@;TrustServerCertificate=True";
-            optionsBuilder.UseSqlServer(connectionstring);
+            var connectionString = "Server=localhost,1433;Database=dotnet7todoappdb;User=sa;Password=kpHm4Cfc@;TrustServerCertificate=True";
+
+            //var envVariableName = _config.GetValue<string>("EnvKeys:TodoDbConn");
+            //var connectionString = Environment.GetEnvironmentVariable(envVariableName);
+            optionsBuilder.UseSqlServer(connectionString);
 
 
         }
