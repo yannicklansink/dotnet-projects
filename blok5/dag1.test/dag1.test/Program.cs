c894505cd3129f6c4a5ef779600602e0bf426544
yannicklansink <yannick.lansink@live.nl>
2023-07-17 11:55:42 +0200

    threading

---
+++ blok5/dag1.test/dag1.test/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace dag1.test
+{
+    internal class Program
+    {
+
+        public static void f(object? o)
+        {
+            if (o == null)
+            {
+                Console.WriteLine("returning function");
+                return;
+            }
+            
+            for (int x = (int)o; x < 100; x++)
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Object lockobject = new Object();
+            lock (lockobject)
+            {
+                // lock 
+            }
+
+            Thread thread = new Thread(f);
+            //thread.Priority = ThreadPriority.Highest;
+            thread.Start(100);
+            f(100);
+
+            Console.WriteLine("Einde main");
+        }
+    }
+}
