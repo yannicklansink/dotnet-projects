078a8fd4e4aaa046817c91320fce759f4d70fdb4
yannicklansink <yannick.lansink@live.nl>
2023-07-20 07:46:12 +0200

    find highest index with 2 threads

---
+++ blok5/dag2.huiswerk/HW.ZoekMaximum/HW.ZoekMaximum/MaxNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW.ZoekMaximum
+{
+    public class MaxNumber
+    {
+        private readonly List<int> numbers;
+        private readonly int start;
+        private readonly int end;
+
+        public int MaxValue { get; set; }
+        public int MaxIndex { get; set; }
+
+        public MaxNumber(List<int> numbers, int start, int end)
+        {
+            this.numbers = numbers;
+            this.start = start;
+            this.end = end;
+        }
+
+        public void FindMax()
+        {
+            MaxValue = numbers[start];
+            MaxIndex = start;
+
+            for (int i = start; i < end; i++)
+            {
+                if (numbers[i] > MaxValue)
+                {
+                    MaxValue = numbers[i];
+                    MaxIndex = i;
+                }
+            }
+        }
+    }
+}
---
+++ blok5/dag2.huiswerk/HW.ZoekMaximum/HW.ZoekMaximum/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HW.ZoekMaximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersList = new List<int> { 1, 22, 3, 4, 2, 18, 9, 12, 15, 5};
+
+            int middleOfList = numbersList.Count / 2;
+
+            MaxNumber findHighestNumber1 = new MaxNumber(numbersList, 0, middleOfList);
+            MaxNumber findHighestNumber2 = new MaxNumber(numbersList, middleOfList, numbersList.Count);
+
+            Thread thread1 = new Thread(findHighestNumber1.FindMax);
+            Thread thread2 = new Thread(findHighestNumber2.FindMax);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            int maxIndex = findHighestNumber1.MaxIndex;
+            if (findHighestNumber2.MaxValue > findHighestNumber1.MaxValue)
+            {
+                maxIndex = findHighestNumber2.MaxIndex;
+                Console.WriteLine("Maximum value index is found in second thread: " + maxIndex);
+            } else 
+            {
+                Console.WriteLine("Maximum value index is found in first thread: " + maxIndex);
+                
+            }
+
+        }
+    }
+}
